d66d63ecfd4dcfe13cd4a5f2c2c2c0d24924ce95
Sami Chaudhary <itssami@gmail.com>
2024-08-02 11:30:00 -0400

    Add project files.

---
+++ FutureValCal/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using FutureValCal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FutureValCal.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FutureVal = "";
+            return View();
+        }
+
+        public IActionResult Index(FutureValModel futureValModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FutureVal = futureValModel.Calculate().ToString("C2");
+            }
+            else
+            {
+                ViewBag.FutureVal = "";
+            }
+
+            return View();
+        }
+    }
+}
---
+++ FutureValCal/Models/FutureValModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FutureValCal.Models
+{
+    public class FutureValModel
+    {
+        [Required(ErrorMessage = "Enter Monthly investment")]
+        [Range(1, 20, ErrorMessage = "Range must be between 1 and 20")]
+        public double? MonthlyInvestment { get; set; }
+
+        [Required(ErrorMessage = "Enter Interest rate investment")]
+        [Range(1, 20, ErrorMessage = "Range must be between 0.1 and 10.0")]
+        public double? YearlyInterestRate { get; set; }
+
+        [Required(ErrorMessage = "Enter YEars")]
+        [Range(1, 20, ErrorMessage = "Range must be between 1 and 20")]
+        public int? Years { get; set; }
+
+        public double Calculate()
+        {
+            int months = Years.Value * 12;
+            double monthlyInterestRate = YearlyInterestRate.Value / 12 / 100;
+            double futureValue = 0;
+
+            for (int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + MonthlyInvestment.Value) * (1 + monthlyInterestRate);
+            }
+
+            return futureValue;
+        }
+    }
+}
